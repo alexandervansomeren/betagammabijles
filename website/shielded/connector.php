cfa01d1076a6cb6b9bd65a7ec273a46c6ebc23f2
Bas Nachtegaal <bas@nachtegaal.eu>
2013-01-22 18:29:05 +0100

    PDO method with accessinformation imported via XML.

--- website/shielded/connector.php
+++ website/shielded/connector.php
@@ -3,6 +3,11 @@ class ConnectorClass
 {
    private $Connection;
    private $ConnectionIniObject;
+   private $ConnectionIniArray;
+   private $databaseserver;
+   private $username;
+   private $password;
+   private $ConnectionHandle;
 
    public function ConnectorClass() {
       echo('connector');
@@ -14,10 +19,16 @@ class ConnectorClass
       $this -> Connection  = 'Test';
       echo(' test ');
       $this -> ConnectionIniObject = simplexml_load_file ( 'shielded/databaseSetup.xml');
-     return;
+      var_dump( $this -> ConnectionIniObject );
+      $this -> ConnectionIniArray = (get_object_vars( $this -> ConnectionIniObject )); 
+      $this -> databaseserver = 'mysql:dbname=' . filter_var( $this -> ConnectionIniArray[ "databasename" ], FILTER_SANITIZE_STRIPPED ) . ';host=' . filter_var( $this -> ConnectionIniArray[ "servername" ], FILTER_SANITIZE_URL ) . ':' . filter_var( $this -> ConnectionIniArray[ "serverport" ], FILTER_SANITIZE_NUMBER_INT ); 
+      echo( $this -> databaseserver );
+      $this -> password = filter_var( $this -> ConnectionIniArray["password"], FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_AMP );
+      $this -> username = filter_var( $this -> ConnectionIniArray["loginname"], FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_AMP );
+
+      try{ $databaseConnection = new PDO( $this -> databaseserver, $this -> username. $this -> password );
+      } catch( PDOExeption $e ){ echo( 'Connection failed: '. $e -> getMessage());    }
+      return;     
    }
 }
 $connectionObject = new ConnectorClass;
-
-?>
-
