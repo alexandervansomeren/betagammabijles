8a5c061194ed31dc3c681a3d0ab9d6e16649e13a
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-24 12:48:40 +0100

    Testing...

--- website/advertenties.php
+++ website/advertenties.php
@@ -231,7 +231,7 @@
 	
 	//Connecting to the database
 	
-	//include 'connectortest.php';
+	include 'connectortest.php';
 	//$connectionObject = new ConnectorClass;
 	
 	$db = new PDO("mysql:host=localhost;dbname=webdb13BG2;charset=UTF-8","webdb13BG2","frerenaz");
