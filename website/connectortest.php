f0a60533f82c065605a07bca1380dc2549300e5e
Bas Nachtegaal <bas@nachtegaal.eu>
2013-01-24 18:39:14 +0100

    different query.

--- website/connectortest.php
+++ website/connectortest.php
@@ -4,7 +4,7 @@ include('shielded/connector.php');
 $connectionObject = new ConnectorClass;
 
 // Test query's here. Should become a set of predefined query's as part of the function Querying().
-$connectionObject -> Query = "SELECT * FROM webdb13BG2.course_difficulty";
+$connectionObject -> Query = "SELECT * FROM webdb13BG2";
 $page = 'Result of the Query ('.$connectionObject -> Query . ') :';
 $page.='<pre>';
 $page .= print_r( $connectionObject -> Querying(), TRUE );
