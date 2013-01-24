a0b018c8fef56c7c7d2a7bd9a147932abce126e9
Bas Nachtegaal <bas@nachtegaal.eu>
2013-01-24 18:12:13 +0100

    Step by step querying.

--- website/connectortest.php
+++ website/connectortest.php
@@ -1,3 +1,15 @@
 <?php
 include('shielded/connector.php');
+$connectionObject = new ConnectorClass;
+
+// Test query's here. Should become a set of predefined query's as part of the function Querying().
+$connectionObject -> Query = "SELECT * FROM webdb13BG2.course_difficulty";
+echo('Result of the Query ('.$connectionObject -> Query.') :');
+echo('<pre>');
+print_r( $connectionObject -> Querying() );
+echo('</pre>');
+// Disconnecting function directed:
+$connectionObject -> Disconnect();
+
+
 ?>
