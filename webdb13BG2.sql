4902d436e2ef5d2f3803e3fdaf7a03558936cae5
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-24 10:24:26 +0100

    Nieuwste versie van de database

--- webdb13BG2.sql
+++ webdb13BG2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 16 jan 2013 om 14:48
+-- Genereertijd: 24 jan 2013 om 10:23
 -- Serverversie: 5.1.61
 -- PHP-versie: 5.3.3
 
@@ -33,7 +33,6 @@ CREATE TABLE IF NOT EXISTS `adress_data` (
   `postal` smallint(6) NOT NULL,
   `postal_extra` varchar(4) CHARACTER SET utf8 NOT NULL,
   `streetnumber` varchar(31) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`user_id`),
   KEY `city` (`city`,`postal`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Lelijke tabel';
 
@@ -42,7 +41,9 @@ CREATE TABLE IF NOT EXISTS `adress_data` (
 --
 
 INSERT INTO `adress_data` (`user_id`, `city`, `street`, `postal`, `postal_extra`, `streetnumber`) VALUES
-(0, 'lkdsjfklsdjf', 'dfjasdfjksd', 1234, 'AB', '12A');
+(10, 'Amsterdam', 'asdf', 1234, 'AB', '12A'),
+(2, 'Amsterdam', 'twee koningskinderenstraat', 1055, 'DJ', '17HS'),
+(4, 'Hall', 'lendeweg', 6964, 'CK', '10');
 
 -- --------------------------------------------------------
 
@@ -54,10 +55,19 @@ CREATE TABLE IF NOT EXISTS `course_code` (
   `course_code` int(11) NOT NULL,
   `course_id` smallint(6) NOT NULL,
   `course_difficulty` smallint(6) NOT NULL,
-  PRIMARY KEY (`course_code`,`course_difficulty`),
+  PRIMARY KEY (`course_code`),
   KEY `course_id` (`course_id`,`course_difficulty`),
   KEY `course_code` (`course_code`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Course_code is combinatie course_id en course_difficulty';
+
+--
+-- Gegevens worden uitgevoerd voor tabel `course_code`
+--
+
+INSERT INTO `course_code` (`course_code`, `course_id`, `course_difficulty`) VALUES
+(1, 1, 1),
+(3, 1, 1),
+(2, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -67,11 +77,28 @@ CREATE TABLE IF NOT EXISTS `course_code` (
 
 CREATE TABLE IF NOT EXISTS `course_difficulty` (
   `difficulty_name` varchar(127) CHARACTER SET utf8 NOT NULL,
-  `difficulty_int` int(11) NOT NULL,
-  UNIQUE KEY `difficulty_int` (`difficulty_int`),
+  `difficulty_id` int(11) NOT NULL,
+  UNIQUE KEY `difficulty_int` (`difficulty_id`),
   UNIQUE KEY `difficulty_name_2` (`difficulty_name`),
-  KEY `difficulty_name` (`difficulty_name`,`difficulty_int`)
+  KEY `difficulty_name` (`difficulty_name`,`difficulty_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='In deze tabel wordt de moeilijkheidsgraad gekoppeld aan een ';
+
+--
+-- Gegevens worden uitgevoerd voor tabel `course_difficulty`
+--
+
+INSERT INTO `course_difficulty` (`difficulty_name`, `difficulty_id`) VALUES
+('Basic', 1),
+('Basisschool', 2),
+('HAVO-bovenbouw', 9),
+('HAVO-onderbouw', 8),
+('PRO', 3),
+('VMBO-BBL', 4),
+('VMBO-GL', 6),
+('VMBO-KBL', 5),
+('VMBO-T', 7),
+('VWO-bovenbouw', 11),
+('VWO-onderbouw', 10);
 
 -- --------------------------------------------------------
 
@@ -81,26 +108,73 @@ CREATE TABLE IF NOT EXISTS `course_difficulty` (
 
 CREATE TABLE IF NOT EXISTS `course_id` (
   `course_name` varchar(127) CHARACTER SET utf8 NOT NULL,
-  `course_int` int(11) NOT NULL,
-  UNIQUE KEY `subject_name_2` (`course_name`,`course_int`),
-  KEY `subject_name` (`course_name`,`course_int`)
+  `course_id` smallint(6) NOT NULL,
+  UNIQUE KEY `subject_name_2` (`course_name`,`course_id`),
+  KEY `subject_name` (`course_name`,`course_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='In deze tabel wordt de vaknaam gekoppeld aan een nummer';
+
+--
+-- Gegevens worden uitgevoerd voor tabel `course_id`
+--
+
+INSERT INTO `course_id` (`course_name`, `course_id`) VALUES
+('Aardrijkskunde', 1),
+('ANW', 19),
+('Arabisch', 25),
+('Biologie', 2),
+('Duits', 11),
+('Economie', 12),
+('Engels', 4),
+('Frans', 10),
+('Fries', 20),
+('Grieks', 14),
+('Informatica', 27),
+('Italiaans', 22),
+('Latijn', 13),
+('M & O', 15),
+('MaL', 16),
+('Muziek', 17),
+('Natuurkunde', 18),
+('Nederlands', 5),
+('NLT', 26),
+('Russisch', 23),
+('Scheikunde', 3),
+('Spaans', 21),
+('Turks', 24),
+('Wiskunde A', 6),
+('Wiskunde B', 7),
+('Wiskunde C', 8),
+('Wiskunde D', 9);
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `course_user_region`
+-- Tabelstructuur voor tabel `course_user`
 --
 
-CREATE TABLE IF NOT EXISTS `course_user_region` (
+CREATE TABLE IF NOT EXISTS `course_user` (
   `course_code` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `region` varchar(63) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `course_code` (`course_code`,`region`),
+  `user_id` int(11) NOT NULL,
+  KEY `course_code` (`course_code`),
   KEY `course_code_2` (`course_code`),
-  KEY `course_code_3` (`course_code`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Region kan uitgebouwd worden met tabel' AUTO_INCREMENT=8 ;
+  KEY `course_code_3` (`course_code`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Region kan uitgebouwd worden met tabel';
+
+--
+-- Gegevens worden uitgevoerd voor tabel `course_user`
+--
+
+INSERT INTO `course_user` (`course_code`, `user_id`) VALUES
+(1, 2),
+(2, 2),
+(1, 4),
+(1, 4),
+(3, 4),
+(1, 6),
+(1, 6),
+(1, 6),
+(1, 10);
 
 -- --------------------------------------------------------
 
@@ -117,7 +191,20 @@ CREATE TABLE IF NOT EXISTS `user_data` (
   UNIQUE KEY `username` (`username`),
   KEY `username_2` (`username`),
   KEY `username_3` (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='In deze tabel wordt de user_data opgeslagen. De primary key ' AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='In deze tabel wordt de user_data opgeslagen. De primary key ' AUTO_INCREMENT=8 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `user_data`
+--
+
+INSERT INTO `user_data` (`user_id`, `username`, `password`, `user_type`) VALUES
+(1, 'Emma123', '1111', 1),
+(2, 'Emma1243', '1111', 1),
+(3, 'alexandervansomeren', 'pw28321', 1),
+(4, 'spacekees', 'tralili', 1),
+(5, 'desuperuser', 'lois', 1),
+(6, 'lauraa-lauu', 'geheim', 1),
+(7, 'EmmaTEST', ' Geheim!', 2);
 
 -- --------------------------------------------------------
 
@@ -139,6 +226,13 @@ CREATE TABLE IF NOT EXISTS `user_personal_data` (
   PRIMARY KEY (`user_id`),
   KEY `gender` (`gender`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `user_personal_data`
+--
+
+INSERT INTO `user_personal_data` (`user_id`, `first_name`, `middle_name`, `last_name`, `gender`, `emailadress`, `phone_1`, `phone_2`, `date_of_birth`, `about_me`) VALUES
+(6, 'laura', 'de', 'helgering', 1, 'laura_helgering@hotmail.com', '612345678', '687654321', '1994-01-25', 'hoi');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
