5fe2bc879e913802311ae121e40fb4aa3094fc91
Bas Nachtegaal <bas@nachtegaal.eu>
2013-01-25 16:04:28 +0100

    Again.

--- website/connectortest.php
+++ website/connectortest.php
@@ -18,23 +18,3 @@ header('Content-Length: ' . $length );
 header('Content-MD5: '. $md5sum );
 echo($page);
 ?>
-~                                                                                                                                
-~
-header('Content-Type: text');
-include('shielded/connector.php');
-$connectionObject = new ConnectorClass;
-
-// Test query's here. Should become a set of predefined query's as part of the function Querying().
-$connectionObject -> Query = "SELECT * FROM webdb13BG2.course_difficulty;";  
-$page = 'Result of the Query ('.$connectionObject -> Query . ') :';
-$page.='<pre>';
-$page .= print_r( $connectionObject -> Querying(), TRUE );
-$page.='</pre>';
-// Disconnecting function directed:
-$connectionObject -> Disconnect();
-$length = strval( strlen ( $page ) );
-$md5sum = md5( $page );
-header('Content-Length: ' . $length );
-header('Content-MD5: '. $md5sum );
-echo($page);
-?>
