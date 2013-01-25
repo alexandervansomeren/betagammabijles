4fbd6848530226a7cf4d42becb0566bc58b095f8
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-25 15:10:47 +0100

    ...

--- website/advertenties.php
+++ website/advertenties.php
@@ -306,7 +306,7 @@
 	$db -> Disconnect();
 	echo "Disconnected";
 	
-	echo $queryResultsArray[0];
+	echo $queryResultsArray[1][user_id];
 	
 	// function that displays that there are no results for the query
 	function showNoResults() 
