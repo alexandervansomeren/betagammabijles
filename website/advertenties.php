d2d783a3b2a0eeb49dd02e16bac2011c2638c159
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-27 11:14:38 +0100

    ..

--- website/advertenties.php
+++ website/advertenties.php
@@ -197,7 +197,7 @@
                         <label class="ques">Man <input type="checkbox" name="male", checked="checked">	</label>
                         <label class="ques">Vrouw <input type="checkbox" name="female", checked="checked">	</label>
                     </div> 
-                    <input class="text" style="margin-top:10px;;" type="submit" value="Zoeken"/>
+                    <input class="part" style="width:60px; margin-left:10px;" type="submit" value="Zoeken"/>
             </form>
         </div>
     <?php
