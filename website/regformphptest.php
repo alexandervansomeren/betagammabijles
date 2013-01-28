d0aaf6f9d10465113869b4189c5f394b1dd78758
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-28 22:59:26 +0100

    ...

--- website/regformphptest.php
+++ website/regformphptest.php
@@ -300,7 +300,8 @@
 	                "   INSERT INTO webdb13BG2.user_data(username, password, user_type) 
 						VALUES (".$GLOBALS['username'].", ".$GLOBALS['password'].", ".$GLOBALS['user_type'].");
 						SELECT user_id FROM webdb13BG2.user_data WHERE username=".$GLOBALS['username'].";";
-						
+					echo $GLOBALS['db'] -> Query;
+					echo ',<br>';	
 					$currentUserArray = $GLOBALS['db'] -> Querying();
 					$currentUserId = $currentUserArray[1][0];
 	                
