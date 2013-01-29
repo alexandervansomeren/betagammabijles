d58c6eeca81817f3a09801cbe2e0d817ea7333f9
A.R. van Someren <alexandervansomeren@MacBook-van-AR-van-Someren.local>
2013-01-29 14:52:17 +0100

    2

--- website/regformphptest.php
+++ website/regformphptest.php
@@ -335,8 +335,8 @@
 
 	                $GLOBALS['db'] -> Query = 
 	                "   INSERT INTO webdb13BG2.user_data(username, password, user_type) 
-						VALUES ("".$GLOBALS['username']."", "".$GLOBALS['password']."", "".$GLOBALS['user_type']."");
-						SELECT user_id FROM webdb13BG2.user_data WHERE username="".$GLOBALS['username']."";";
+						VALUES (".$GLOBALS['username'].", ".$GLOBALS['password'].", ".$GLOBALS['user_type'].");
+						SELECT user_id FROM webdb13BG2.user_data WHERE username=".$GLOBALS['username'].";";
 					echo $GLOBALS['db'] -> Query;
 					echo ',<br>';	
 					$currentUserArray = $GLOBALS['db'] -> Querying();
