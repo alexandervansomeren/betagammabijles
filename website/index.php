3a981e03c725e9b39a74586879ac1d0554f0c397
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-29 12:51:42 +0100

    ......

--- website/index.php
+++ website/index.php
@@ -12,7 +12,7 @@ echo "poep lars";
 	<html>
 =======
 <?php 
-$page = '<?xml version="1.0" encoding="UTF-8"?>
+$page = '123<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
