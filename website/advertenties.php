b1fe2177d9871b84ee88bd817fe6072452fb25b5
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-30 15:11:45 +0100

    ...

--- website/advertenties.php
+++ website/advertenties.php
@@ -187,10 +187,11 @@
         </div>
     </div>
 	<?php 
-	echo 'user type als sessie werkt: ';
+	session_start();
+	echo 'user type als sessie werkt: <br />';
 	if ( isset( $_SESSION['user_type'] ) )
 	{
-		echo "Je bent ingelogt als".$_SESSION['user_type'];
+		echo "Je bent ingelogt als ".$_SESSION['user_type'];
 	}
 	?>
     <div class="content">
