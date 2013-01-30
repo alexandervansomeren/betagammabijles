ea7ec04345214a0af2019436d24562a4d9e1cd6b
A.R. van Someren <alexandervansomeren@wcw-stud-68-33.wireless.uva.nl>
2013-01-30 23:34:21 +0100

    Lelijke laatste versie

--- website/wachtwoordcheck.php
+++ website/wachtwoordcheck.php
@@ -1,16 +1,19 @@
 <?php
+	function lifeQuery( $username )
+	{
     // Connect to the database
     include 'shielded/connector.php';
     $db = new ConnectorClass;
-    $usern = mysql_real_escape_string($_POST['username']);
+    $usern = mysql_real_escape_string( $username );
     $GLOBALS['db'] -> Query =   
     "
     SELECT user_name FROM webdb13BG2.user_data WHERE username='".$usern."';
     ";
     $userSame = $GLOBALS['db'] -> Querying();
-    if(!(empty($userSame)))
+    if( isset($userSame[1][0] )
     {
-       unCheck();
+       return( true );
     }
     $GLOBALS['db'] -> Disconnect(); 
+    }
 ?>
