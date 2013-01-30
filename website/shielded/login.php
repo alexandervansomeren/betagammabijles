8293332e98a3fb269bfd29295ec110b78ca12699
unknown <tim.leunissen@43WN4S1.OskomeraGroup.local>
2013-01-31 00:22:59 +0100

    sadf

--- website/shielded/login.php
+++ website/shielded/login.php
@@ -79,9 +79,7 @@ function login( $username, $shaPassword )
 		{
                         $setLastLogin = new ConnectorClass;
                         $setLastLogin -> Query = 
-                	"
-                        UPDATE webdb13BG2.user_personal_data SET last_login ='" .  Date('Y-m-d H:i:s') . "' WHERE user_personal_data.user_id = 2;
-                        ";
+                	"UPDATE webdb13BG2.user_personal_data SET last_login ='" .  Date('Y-m-d H:i:s') . "' WHERE user_personal_data.user_id = 2";
                         echo $setLastLogin -> Query;
                         $resultOfQ = $setLastLogin -> Querying();
                         
