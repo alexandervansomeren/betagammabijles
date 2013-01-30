e136b5c9efa03833707f7f84b9dc4c6a1f8d7cf3
unknown <tim.leunissen@43WN4S1.OskomeraGroup.local>
2013-01-30 16:55:54 +0100

    asdf

--- website/index.php
+++ website/index.php
@@ -22,14 +22,14 @@ if (sizeOf( $GLOBALS['queryResultsArray'] ) >= 1)
         {
             echo "hier kwam ik nog!! echt:". $x ." <br />";
             // Get vakken die docent geeft
-            $GLOBALS['db'] -> Query = 
+            $GLOBALS['db123'] -> Query = 
             'SELECT ci.course_name
             FROM webdb13BG2.course_user cu
             INNER JOIN webdb13BG2.course_code cc ON cc.course_code = cu.course_code 
             INNER JOIN webdb13BG2.course_id ci ON cc.course_id = ci.course_id 
             WHERE cu.user_id = '. $vakRow['user_id'] .';';
             
-            $GLOBALS['queryResultsArray1'] = $GLOBALS['db'] -> Querying();
+            $GLOBALS['queryResultsArray1'] = $GLOBALS['db123'] -> Querying();
             $GLOBALS['vakken'] = "";
             
             print_r($GLOBALS['queryResultsArray1']);
