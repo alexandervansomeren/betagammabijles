65163f08c03287769087f365a942cb2c37dc79fd
unknown <tim.leunissen@43WN4S1.OskomeraGroup.local>
2013-01-30 21:40:09 +0100

    asdf

--- website/index.php
+++ website/index.php
@@ -6,34 +6,34 @@
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
-<body>
+<body onload="initialize()">
     <div class="header">
 	    <div class="center">
         	<div class="left"> <a href="index.html" class="left"> Waarbijles1 </a> </div>
             <div class="middle"></div>
             <div class="right">
             	<?php 
-				session_start();
-				if ( isset($_SESSION['user_type']) )
-					{
-					if ( is_int( $_SESSION['user_type'] ) )
-					{
-						echo '<div class="text"><p> Je bent ingelogt als '.$_SESSION['user_type'];
-						echo '</p>';
-						echo '<p><a href="logout.php">Uitloggen</a></p></div>';
-					}
-				}
-				else echo 
-				'<form method="post" action="logincheck.php">
-					<div class="label">Voor leden geef je gegevens en log in
-					</div>
-					<div class="login">
-						<input type="text" placeholder="Gebruikersnaam" name="username"/> 
-						<input type="password" placeholder="Wachtwoord" name="password"/>
-						<button type="submit">Login</button>
-					</div>
-				</form>';
-				?>
+                    session_start();
+                    if ( isset($_SESSION['user_type']) )
+                            {
+                            if ( is_int( $_SESSION['user_type'] ) )
+                            {
+                                    echo '<div class="text"><p> Je bent ingelogt als '.$_SESSION['user_type'];
+                                    echo '</p>';
+                                    echo '<p><a href="logout.php">Uitloggen</a></p></div>';
+                            }
+                    }
+                    else echo 
+                    '<form method="post" action="logincheck.php">
+                            <div class="label">Voor leden geef je gegevens en log in
+                            </div>
+                            <div class="login">
+                                    <input type="text" placeholder="Gebruikersnaam" name="username"/> 
+                                    <input type="password" placeholder="Wachtwoord" name="password"/>
+                                    <button type="submit">Login</button>
+                            </div>
+                    </form>';
+                ?>
             </div>
         </div>
     </div>
