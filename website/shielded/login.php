d1950d59b87bb341ede2988d7299c9896c3bdb76
Alexander <alexander.vansomeren@student.uva.nl>
2013-01-31 19:34:37 +0100

    ..

--- website/shielded/login.php
+++ website/shielded/login.php
@@ -1,55 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Login</title>
-</head>
-<body>
 <?php
-
-// // use this for password generation:
-// include 'shielded/login.php';
-// $passwordsha=sha1($password);  // $password is the password that the user chose
-// $passwordArray = createPasswordSalt($passwordsha);
-// $DBpassword = $passwordArray['password']; // this is the actual password value suited for the database
-// $DBsalt = $passwordArray['salt']; // this is the actual salt value suited for the database
-
-/*
-$username = 'alexsomer';
-$password = 'w8woord';
-$shaPassword = sha1( $password );
-$pwSaltarray = createPasswordSalt( $shaPassword );
-$DBpassword = $passwordArray['password']; // this is the actual password value suited for the database
-$DBsalt = $passwordArray['salt']; // this is the actual salt value suited for the database
-
-include 'connector.php';
-$db = new ConnectorClass;
-
-$db -> Query = 
-	"
-		INSERT INTO webdb13BG2.user_data
-		(username, password, salt)
-		VALUES ('".$username."','".$DBpassword."' ,'".$DBsalt."' );
-	";
-echo $db -> Query;
-echo '<br />';	
-$db -> Querying();	
-$db -> Disconnect();
-echo '<br />';
-echo login( $username, $shaPassword );
-*/
-
-
+// Function that creates the encrypted password and the salt for the database 
 function createPasswordSalt( $shaPassword )
 {
 	$salt = md5( uniqid(rand(), true) );
 	$salted = sha1( $salt );
-	$pwForDatabase = sha1( $salted.$shaPassword.$salt );
+	$pwForDatabase = sha1( $salted.$shaPassword.$salt ); // this is where the actual excrypted password is generated
 	$PwSaltArray = array();
 	$PwSaltArray[0] = $pwForDatabase;
 	$PwSaltArray[1] = $salt;
 	return( $PwSaltArray );
 }
+
+// This function checks a username-password combination with the database
+// it uses a SHA-1 encrypted password as parameter for extra security
 function login( $username, $shaPassword )
 {
 	$dbLOGIN = new ConnectorClass;
@@ -60,33 +23,29 @@ function login( $username, $shaPassword )
 			FROM webdb13BG2.user_data
 			WHERE webdb13BG2.user_data.username='".$username."';
 		";
-	//echo $dbLOGIN -> Query;
 	$queryResultArray = $dbLOGIN -> Querying();
-	//echo '<pre>';
-	//print_r($queryResultArray);
-	//echo'</pre>';
 	$dbLOGIN -> Disconnect();
         unset($dbLOGIN); 
-	if ( isset($queryResultArray[1][0]) && isset($queryResultArray[1][1]) && isset($queryResultArray[1][2]))
-		{
+	if ( isset($queryResultArray[1][0]) && isset($queryResultArray[1][1]) && isset($queryResultArray[1][2]) )
+	{
 		$pw = $queryResultArray[1][0];
 		$salt = $queryResultArray[1][1];
 		$user_type = $queryResultArray[1][2];
-		// $user_id = $queryResultArray[1][3];  // For giving user_id to 
+		// $user_id = $queryResultArray[1][3];  // Should be used when a session wants to remember the use_id as well
 		$salted = sha1( $salt );
 		$tryPW = sha1( $salted.$shaPassword.$salt );
 		if ( $pw == $tryPW )
 		{
-                        //$setLastLogin = new ConnectorClass;
-                        //$setLastLogin -> Query = 
-                        //"UPDATE webdb13BG2.user_personal_data SET last_login = '1234' WHERE user_id = '3'";       
-                        //echo $setLastLogin -> Query;
-                        //$resultOfQ = $setLastLogin -> Querying();
-                        
-                        //print_r($resultOfQ);
-						$user_typeINT = intval( $user_type );
-                        return ( $user_typeINT );
-                }
+			//$setLastLogin = new ConnectorClass;
+			//$setLastLogin -> Query = 
+			//"UPDATE webdb13BG2.user_personal_data SET last_login = '1234' WHERE user_id = '3'";       
+			//echo $setLastLogin -> Query;
+			//$resultOfQ = $setLastLogin -> Querying();
+			
+			//print_r($resultOfQ);
+			$user_typeINT = intval( $user_type );
+			return ( $user_typeINT );
+      	}
 	}
 	else 
 	{	
@@ -94,5 +53,3 @@ function login( $username, $shaPassword )
 	}
 }
 ?>
-</body>
-</html>
