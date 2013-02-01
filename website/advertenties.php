1e36c67e1e65bfcde661986b49d93ff47de8a85c
A.R. van Someren <alexandervansomeren@wcw-stud-70-183.wireless.uva.nl>
2013-02-01 15:46:45 +0100

    a

--- website/advertenties.php
+++ website/advertenties.php
@@ -31,170 +31,173 @@
     // Initializing variables and secure that they are not mysql-injections
     if (isset($_GET['course']))
     {
-            $course = mysql_real_escape_string(strip_tags($_GET['course']));
+        $course = mysql_real_escape_string(strip_tags($_GET['course']));
     }
     else $course="";
     if (isset($_GET['city']))
     {
-            $city = mysql_real_escape_string(strip_tags($_GET['city']));
+        $city = mysql_real_escape_string(strip_tags($_GET['city']));
     }
     else $city="";
     if (isset($_GET['level']))
     {
-            $level = mysql_real_escape_string(strip_tags($_GET['level']));
+        $level = mysql_real_escape_string(strip_tags($_GET['level']));
     }
     else $level="";
 
     if (isset($_GET['male']))
     {
-            $male="1";
+        $male="1";
     }
     else $male="0";
     if (isset($_GET['female']))
     {
-            $female="1";
+        $female="1";
     }
     else $female="0";
 	
 	// This creates the part of the query that selects on gender 
     if ($male=="1" && $female=="1")
     {
-            $genderQuery = "";
-            makeFirstQuery();
+        $genderQuery = "";
+        makeFirstQuery();
     }
     else if ($male=="1")
     {
-            $genderQuery = "AND up.gender=1";
-            makeFirstQuery();
+        $genderQuery = "AND up.gender=1";
+        makeFirstQuery();
     }
     else if ($female=="1")
     {
-            $genderQuery = "AND up.gender=0";
-            makeFirstQuery();
+        $genderQuery = "AND up.gender=0";
+        makeFirstQuery();
     }
     else
     {
-            echo 
-            '
-            <div class="page-field"> 
-                    <h1> Er zijn geen bijlesgevers gevonden in onze database die voldoen aan de door u opgegeven criterea </h1>
-            </div>
-            ';
+        echo 
+        '
+        <div class="page-field"> 
+            <h1> Er zijn geen bijlesgevers gevonden in onze database die voldoen aan de door u opgegeven criterea </h1>
+        </div>
+        ';
     }
 
     // Making first query to find user_id's from submitted inputform
     function makeFirstQuery()
     {
-            $GLOBALS['db'] -> Query = 
-            "
-                    SELECT DISTINCT
-                    ad.user_id AS user_id,
-                    ad.city AS user_city,
-                    cu.course_code AS course_id, 
-                    ci.course_name AS course_name,
-                    cd.difficulty_name AS course_difficulty
+        $GLOBALS['db'] -> Query = 
+        "
+        SELECT DISTINCT
+        ad.user_id AS user_id,
+        ad.city AS user_city,
+        cu.course_code AS course_id, 
+    	ci.course_name AS course_name,
+        cd.difficulty_name AS course_difficulty
 
-                    FROM webdb13BG2.adress_data ad 
-                    INNER JOIN webdb13BG2.course_user cu ON cu.user_id = ad.user_id 
-                    INNER JOIN webdb13BG2.course_code cc ON cc.course_code = cu.course_code 
-                    INNER JOIN webdb13BG2.course_id ci ON cc.course_id = ci.course_id 
-                    INNER JOIN webdb13BG2.course_difficulty cd ON cd.difficulty_id = cc.course_difficulty
-                    INNER JOIN webdb13BG2.user_personal_data up ON up.user_id = cu.user_id 
+        FROM webdb13BG2.adress_data ad 
+        INNER JOIN webdb13BG2.course_user cu ON cu.user_id = ad.user_id 
+        INNER JOIN webdb13BG2.course_code cc ON cc.course_code = cu.course_code 
+        INNER JOIN webdb13BG2.course_id ci ON cc.course_id = ci.course_id 
+        INNER JOIN webdb13BG2.course_difficulty cd ON cd.difficulty_id = cc.course_difficulty
+        INNER JOIN webdb13BG2.user_personal_data up ON up.user_id = cu.user_id 
 
-                    WHERE ci.course_name LIKE '%". $GLOBALS['course'] ."%' 
-                    AND 
-                    ad.city LIKE '%". $GLOBALS['city'] ."%' 
-                    AND 
-                    cd.difficulty_name LIKE '%". $GLOBALS['level'] ."%'
-                    ". $GLOBALS['genderQuery'] .";
-            ";
+        WHERE ci.course_name LIKE '%". $GLOBALS['course'] ."%' 
+        AND 
+        ad.city LIKE '%". $GLOBALS['city'] ."%' 
+        AND 
+    	cd.difficulty_name LIKE '%". $GLOBALS['level'] ."%'
+        ". $GLOBALS['genderQuery'] .";
+        ";
 
-            // Getting results from query
-            $GLOBALS['queryResultsArray'] = $GLOBALS['db'] -> Querying();
+        // Getting results from query
+        $GLOBALS['queryResultsArray'] = $GLOBALS['db'] -> Querying();
 
-            // checking whether any results where found, if found, call a function to find the courses and locations from the user_id
-            if (sizeOf( $GLOBALS['queryResultsArray'] ) ==0)
-            {
-                    showNoResults();
-            }
-            else makeSecondQuery();
+        // checking whether any results where found, if found, call a function to find the courses and locations from the user_id
+        if (sizeOf( $GLOBALS['queryResultsArray'] ) ==0)
+        {
+            showNoResults();
+        }
+        else
+        {
+        	makeSecondQuery();
+    	}
     }
-
-    //echo('<pre>');
-    //print_r( $queryResultsArray );
-    //echo('</pre>');
-
 
     function makeSecondQuery()
     {
-            for($i=1;$i<=sizeof($GLOBALS['queryResultsArray']); $i++)
-            {
-                    $users[$i]="webdb13BG2.course_user.user_id=" . $GLOBALS['queryResultsArray'][$i][0];
-            }
-            $usersQuery=implode(" OR ", $users);
+        for($i=1;$i<=sizeof($GLOBALS['queryResultsArray']); $i++)
+        {
+            $users[$i]="webdb13BG2.course_user.user_id=" . $GLOBALS['queryResultsArray'][$i][0];
+        }
+        $usersQuery=implode(" OR ", $users);
 
-            $GLOBALS['db'] -> Query = 
-            "
-                    SELECT course_user.user_id, course_name  
-                    FROM webdb13BG2.course_user 
-                    INNER JOIN webdb13BG2.course_code ON webdb13BG2.course_code.course_code=webdb13BG2.course_user.course_code 
-                    INNER JOIN webdb13BG2.course_id on webdb13BG2.course_code.course_id=webdb13BG2.course_id.course_id 
-                    WHERE ".$usersQuery."
-                    ORDER BY course_user.user_id
-            ;";
-            $courseNamesArray = $GLOBALS['db'] -> Querying();
+        $GLOBALS['db'] -> Query = 
+        "
+        SELECT course_user.user_id, course_name  
+        FROM webdb13BG2.course_user 
+        INNER JOIN webdb13BG2.course_code ON webdb13BG2.course_code.course_code=webdb13BG2.course_user.course_code 
+        INNER JOIN webdb13BG2.course_id on webdb13BG2.course_code.course_id=webdb13BG2.course_id.course_id 
+        WHERE ".$usersQuery."
+        ORDER BY course_user.user_id
+        ;";
+        
+        $courseNamesArray = $GLOBALS['db'] -> Querying();
+        
+        //Resetting
+        $GLOBALS['db'] -> QueryResult = null;
+        
+        //New Query
+        $GLOBALS['db'] -> Query = 
+        "
+        SELECT DISTINCT course_user.user_id, first_name, middle_name, last_name, city  
+        FROM webdb13BG2.course_user 
+        INNER JOIN webdb13BG2.adress_data on webdb13BG2.course_user.user_id=webdb13BG2.adress_data.user_id 
+        INNER JOIN webdb13BG2.user_personal_data on webdb13BG2.course_user.user_id=webdb13BG2.user_personal_data.user_id
+        WHERE ".$usersQuery."
+        ;";
 
-            $GLOBALS['db'] -> QueryResult = null;
-            $GLOBALS['db'] -> Query = 
-            "
-                    SELECT DISTINCT course_user.user_id, first_name, middle_name, last_name, city  
-                    FROM webdb13BG2.course_user 
-                    INNER JOIN webdb13BG2.adress_data on webdb13BG2.course_user.user_id=webdb13BG2.adress_data.user_id 
-                    INNER JOIN webdb13BG2.user_personal_data on webdb13BG2.course_user.user_id=webdb13BG2.user_personal_data.user_id
-                    WHERE ".$usersQuery."
-            ;";
+        $nameCityArray = $GLOBALS['db'] -> Querying();
 
-            $nameCityArray = $GLOBALS['db'] -> Querying();
-
-            // Disconnect from the database
-            $GLOBALS['db']  -> Disconnect();
+        // Disconnect from the database
+        $GLOBALS['db']  -> Disconnect();
 			
-            // Create an array with unique user_id's from selection
-            for($i=1;$i<=sizeof($nameCityArray); $i++)
-            {
-                    $user[$i-1]=$currentUser=$nameCityArray[$i][0];
-            }
-            $user=array_unique($user);
+        // Create an array with unique user_id's from selection
+        for($i=1;$i<=sizeof($nameCityArray); $i++)
+        {
+            $user[$i-1]=$currentUser=$nameCityArray[$i][0];
+        }
+        $user=array_unique($user);
 
-            // Create an array with user_ids as keys and an array with course_names as values
-            for($i=0;$i<sizeof($user); $i++)
-            {
-                    $coursesPerUser[$user[$i]]=array();
-                    for($j=1;$j<=sizeof($courseNamesArray); $j++)
-                    if ($courseNamesArray[$j][0]==$user[$i])
-                    {
-                            array_push($coursesPerUser[$user[$i]], $courseNamesArray[$j][1]);
-                    }
-            }
-            echo '<div class="page-field">';
-            for($i=1;$i<=sizeof($nameCityArray); $i++)
-            {
-                    printVisitCard(
-                            $nameCityArray[$i][0],
-                            $nameCityArray[$i][1],
-                            $nameCityArray[$i][2],
-                            $nameCityArray[$i][3],
-                            $nameCityArray[$i][4],
-                            $coursesPerUser[$nameCityArray[$i][0]]
-                            );
-            }
-
+        // Create an array with user_ids as keys and an array with course_names as values
+        for($i=0;$i<sizeof($user); $i++)
+        {
+            $coursesPerUser[$user[$i]]=array();
+            for($j=1;$j<=sizeof($courseNamesArray); $j++)
+                if ($courseNamesArray[$j][0]==$user[$i])
+                {
+                	array_push($coursesPerUser[$user[$i]], $courseNamesArray[$j][1]);
+                }
+        }
+        
+        //Printing every users VisitCard
+        echo '<div class="page-field">';
+        for($i=1;$i<=sizeof($nameCityArray); $i++)
+        {
+            printVisitCard(
+                $nameCityArray[$i][0],
+                $nameCityArray[$i][1],
+                $nameCityArray[$i][2],
+                $nameCityArray[$i][3],
+                $nameCityArray[$i][4],
+                $coursesPerUser[$nameCityArray[$i][0]]
+            );
+        }
     }
 
     // function that displays that there are no results for the query
     function showNoResults() 
     {
-            echo 
+        echo 
             '
             <div class="page-field"> 
                     <h1> Er zijn helaas (nog) geen bijlesgevers die '. $GLOBALS['course'] .' geven in '. $GLOBALS['city'] .'. </h1>
