4408cbb03f07714da9584476f3f92d58d7f5c3e8
Alexander <alexander.vansomeren@student.uva.nl>
2013-02-01 14:48:24 +0100

    ….

---
+++ website/mailtest.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Mailtest</title>
+</head>
+
+<?php
+function spamcheck($field)
+  {
+  //filter_var() sanitizes the e-mail
+  //address using FILTER_SANITIZE_EMAIL
+  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+
+  //filter_var() validates the e-mail
+  //address using FILTER_VALIDATE_EMAIL
+  if(filter_var($field, FILTER_VALIDATE_EMAIL))
+    {
+    return TRUE;
+    }
+  else
+    {
+    return FALSE;
+    }
+  }
+
+if (isset($_REQUEST['email']))
+  {//if "email" is filled out, proceed
+
+  //check if the email address is invalid
+  $mailcheck = spamcheck($_REQUEST['email']);
+  if ($mailcheck==FALSE)
+    {
+    echo "Invalid input";
+    }
+  else
+    {//send email
+    $email = $_REQUEST['email'] ;
+    $subject = $_REQUEST['subject'] ;
+    $message = $_REQUEST['message'] ;
+    mail("someone@example.com", "Subject: $subject",
+    $message, "From: $email" );
+    echo "Thank you for using our mail form";
+    }
+  }
+else
+  {//if "email" is not filled out, display the form
+  echo "<form method='post' action='mailform.php'>
+  Email: <input name='email' type='text'><br>
+  Subject: <input name='subject' type='text'><br>
+  Message:<br>
+  <textarea name='message' rows='15' cols='40'>
+  </textarea><br>
+  <input type='submit'>
+  </form>";
+  }
+?>
+
+
+<body>
+</body>
+</html>
