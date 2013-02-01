37fa1333fb562f3f4d6e13d81883a40150d443bf
Emma Boumans <10194851@u030383.fnwi.uva.nl>
2013-02-01 10:43:27 +0100

    aa

--- website/regformphptest.php
+++ website/regformphptest.php
@@ -345,28 +345,13 @@
                 {
                     //Connecting to the database and querying
                     makeQuery();
-                    makeVakkenQuery(); 
                     disconnectGoodbye();
                 }
             } else {
                 //Connecting to the database and querying
-                makeQuery();
                 disconnectGoodbye();
             }
-        }
-    
-        function enterCourseCode($course_int)
-        {
-                $ui_int = intval($GLOBALS['user_id']);
-                $GLOBALS['db'] -> Query =
-                    "
-                    INSERT INTO webdb13BG2.course_user (course_code, user_id) 
-                    VALUES (".$course_int.", ".$ui_int.");
-                    ";
-                $GLOBALS['db'] -> Querying();
-                
-                $GLOBALS['db'] -> Query = null;
-                $GLOBALS['db'] -> QueryResult = null;
+            header('Location: regform3.php');
         }
     
         // Making query
@@ -417,54 +402,6 @@
                 VALUES ('".$GLOBALS['user_id']."', '".$GLOBALS['city']."', '".$GLOBALS['street']."', '".$GLOBALS['streetnumber']."', '".$GLOBALS['postal']."', '".$GLOBALS['postal_extra']."');
                 ";
             $GLOBALS['db'] -> Querying();      
-        }
-        
-        function makeVakkenQuery()
-        {
-            $N = count($GLOBALS['courseTest']);
-            $localCourse = array();
-            $localCourse = $GLOBALS['courseTest'];
-            for($i=0; $i < $N; $i++)
-            {
-                $course_code = $localCourse[$i];
-                makeOption($course_code);
-            }
-        }
-        
-        function makeOption($course_code)
-        {
-    		$course_code = $course_code*100 + 11;
-    	    $course_int = intval($course_code);
-    	    makeOptionVisible($course_int);       	
-        }
-        
-        function makeOptionVisible($course_int)
-        {
-            echo '<div class="page-field">';
-                echo "Je hebt een vak ingevoerd: ";
-                echo $course_int;
-                echo '<form method="post">
-                        <div class="ques">
-                            Op welk niveau wil je dit vak geven?
-                        </div>
-                        <div class="ans">
-                            <select name="niveau">
-                                <option value="1">VMBO</option>
-                                <option value="2">VMBOT</option>
-                                <option value="3">HAVO</option>
-                                <option value="4">VWO</option>
-                            </select>
-                        </div>
-                        <div class="field">
-                            <input type="submit" value="Kies" style="background-color:orange;" name="submit" />
-                        </div>
-                      </form>';
-                if(isset($_POST['niveau']))
-                {
-                    echo $_POST['niveau'];
-                    enterCourseCode($course_int);
-                }
-            echo '</div>';
         }
         
         function disconnectGoodbye()
